2f6ff9f2091337a227409d5f671c21837bde04f1
Leo <asdfpeng@qq.com>
2026-07-09 16:44:48 +0800

    Deploy site

---
+++ assets/chunks/useReporter.D4n0v9NN.js
@@ -0,0 +1,1 @@
+import{y as e,a2 as n}from"./framework.BRCYMVIo.js";function i(){async function o(){const t=document.title;await fetch("https://iicoom.duckdns.org/api/visitor",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({referrer:document.referrer||"直接打开",title:t})})}e(()=>{window.location.hostname!=="localhost"&&o()}),n(()=>{})}export{i as u};
